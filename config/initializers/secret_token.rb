69f5d0744be32360030fa8404419f6a27fb4bc4e
John King <John@thumbtacksoftware.com>
2013-05-24 15:03:12 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskRabbit::Application.config.secret_token = 'afa756650d50d1ca7e662f48015dfdc1256c19bddd8f4d9f6c52b20957151a2fd25126c18a7dc75d9757c3cab341e3644f37323d1ea4cfaf98da96bd1e1e0e11'
